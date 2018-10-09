09b8d9f193f307b0f0485237b4ab8de659fb152a
davidlawes <davidlawes@hotmail.co.uk>
2018-10-09 16:21:00 +0100

    test: DRY out feature tests

--- js/spec/features/backstage_passes_features_spec.js
+++ js/spec/features/backstage_passes_features_spec.js
@@ -11,7 +11,6 @@ describe("Feature: Backstage Passes", function() {
   it("when '5 < sellIn <= 10', increases the quality by 2", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 8) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
     expect(items[0].sellIn).toEqual(9);
     expect(items[0].quality).toEqual(10);
   });
@@ -19,7 +18,6 @@ describe("Feature: Backstage Passes", function() {
   it("when '0 <= sellIn <= 5', increases the quality by 3", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 8) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
     expect(items[0].sellIn).toEqual(4);
     expect(items[0].quality).toEqual(11);
   });
@@ -27,7 +25,6 @@ describe("Feature: Backstage Passes", function() {
   it("when 'sellIn < 0', quality equals 0", function() {
     const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 8) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
     expect(items[0].sellIn).toEqual(-1);
     expect(items[0].quality).toEqual(0);
   });
--- js/spec/features/normal_items_features_spec.js
+++ js/spec/features/normal_items_features_spec.js
@@ -9,12 +9,10 @@ describe("Feature: Normal Items", function() {
   });
 
   it("caps the lowest quality at 0", function() {
-    const gildedRose = new Shop([ new Item("Bread", 5, 1) ]);
+    const gildedRose = new Shop([ new Item("Bread", 5, 0) ]);
     const items = gildedRose.updateQuality();
-    const gildedRose2 = new Shop(items)
-    const items2 = gildedRose2.updateQuality();
-    expect(items2[0].sellIn).toEqual(3);
-    expect(items2[0].quality).toEqual(0);
+    expect(items[0].sellIn).toEqual(4);
+    expect(items[0].quality).toEqual(0);
   });
 
   it("reduces the quality twice as quickly when sellIn < 0", function() {
