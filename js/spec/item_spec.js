5d6a7d0a0ffc1bda1f73769741ab71e1e48c1277
davidlawes <davidlawes@hotmail.co.uk>
2018-10-09 16:11:06 +0100

    test: added extra tests to Item class

--- js/spec/item_spec.js
+++ js/spec/item_spec.js
@@ -1,8 +1,23 @@
-describe("Unit: Item", function() {
+describe("Item", function() {
 
   it("constructs an Item prototype", function() {
     const item = new Item("Bread", 5, 8);
     expect(item.constructor).toEqual(Item);
+  });
+
+  it("returns the name", function() {
+    const item = new Item("Bread", 5, 8);
+    expect(item.name).toEqual("Bread");
+  });
+
+  it("returns the sellIn", function() {
+    const item = new Item("Bread", 5, 8);
+    expect(item.sellIn).toEqual(5);
+  });
+
+  it("returns the quality", function() {
+    const item = new Item("Bread", 5, 8);
+    expect(item.quality).toEqual(8);
   });
 
 });
