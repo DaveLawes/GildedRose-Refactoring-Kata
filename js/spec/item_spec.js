a501898e860690146dc09a74d5d8eef7c414634f
davidlawes <davidlawes@hotmail.co.uk>
2018-10-09 16:15:19 +0100

    test: DRY out item_spec

--- js/spec/item_spec.js
+++ js/spec/item_spec.js
@@ -1,22 +1,24 @@
 describe("Item", function() {
 
+  var item;
+
+  beforeEach(function() {
+    item = new Item("Bread", 5, 8);
+  });
+
   it("constructs an Item prototype", function() {
-    const item = new Item("Bread", 5, 8);
     expect(item.constructor).toEqual(Item);
   });
 
   it("returns the name", function() {
-    const item = new Item("Bread", 5, 8);
     expect(item.name).toEqual("Bread");
   });
 
   it("returns the sellIn", function() {
-    const item = new Item("Bread", 5, 8);
     expect(item.sellIn).toEqual(5);
   });
 
   it("returns the quality", function() {
-    const item = new Item("Bread", 5, 8);
     expect(item.quality).toEqual(8);
   });
 
