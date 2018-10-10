f15f2f675b7bdbd93b69bb0b0ff36f4ec59d37a8
davidlawes <davidlawes@hotmail.co.uk>
2018-10-10 17:46:18 +0100

    test: added tests for Conjured items

---
+++ js/spec/UpdateConjuredSpec.js
@@ -0,0 +1,23 @@
+describe("Update Conjured Item", function() {
+
+  it("decreases sellIn by 1", function() {
+    item = new Item("Conjured Mana Cake", 1, 4)
+    expect(updateConjuredItem(item).sellIn).toEqual(0);
+  });
+
+  it("decreases quality by 2 when sellIn >= 0", function() {
+    item = new Item("Conjured Mana Cake", 1, 4)
+    expect(updateConjuredItem(item).quality).toEqual(2);
+  });
+
+  it("decreases quality by 4 when sellIn < 0", function() {
+    item = new Item("Conjured Mana Cake", 0, 5)
+    expect(updateConjuredItem(item).quality).toEqual(1);
+  });
+
+  it("caps lowest quality at 0", function() {
+    item = new Item("Conjured Mana Cake", 1, 1)
+    expect(updateConjuredItem(item).quality).toEqual(0);
+  });
+
+});
--- js/spec/features/NormalItemsFeatureSpec.js
+++ js/spec/features/NormalItemsFeatureSpec.js
@@ -2,7 +2,7 @@
 
 describe("Feature: Normal Items", function() {
 
-  it("reduces the quality and sellIn value every time .updateQuality() is invoked", function() {
+  it("when sellIn >= 0, reduces the quality by 1", function() {
     const gildedRose = new Shop([ new Item("Bread", 5, 8) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("Bread");
@@ -17,7 +17,7 @@ describe("Feature: Normal Items", function() {
     expect(items[0].quality).toEqual(0);
   });
 
-  it("reduces the quality twice as quickly when sellIn < 0", function() {
+  it("when sellIn < 0, reduces the quality by 2", function() {
     const gildedRose = new Shop([ new Item("Bread", 0, 8) ]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).toEqual(-1);
