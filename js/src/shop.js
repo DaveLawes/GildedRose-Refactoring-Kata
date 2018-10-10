248a35f5ace8d0bf32c9904c44661f204f235172
davidlawes <davidlawes@hotmail.co.uk>
2018-10-10 14:25:23 +0100

    refactor: removed if statements from ._isNormal function

--- js/src/shop.js
+++ js/src/shop.js
@@ -1,56 +1,66 @@
+"use strict";
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this._isNormal(this.items[i].name)) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
 
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+      const item = this.items[i] // use item throughout case statement for ease of reading
+
+      this._updateSellIn(item)
+
+      switch(true) {
+
+        case(this._isNormal(item.name)):
+          item.quality === 0 ? item.quality = 0
+          : item.sellIn >= 0 ? item.quality -= 1
+                             : item.quality -= 2
+          break;
+
+        case(this._isAgedBrie(item.name)):
+          if (item.quality < 50 && item.sellIn >= 0) {
+            item.quality = item.quality + 1;
+          } else if (item.quality < 50 && item.sellIn < 0) {
+            item.quality = item.quality + 2;
+          }
+          break;
+
+        case(this._isBackstagePasses(this.items[i].name)):
+          if (this.items[i].quality < 50) {
+            if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+              if (this.items[i].sellIn < 11) {
+                if (this.items[i].quality < 50) {
+                  this.items[i].quality = this.items[i].quality + 1;
+                }
+              }
+              if (this.items[i].sellIn < 6) {
+                if (this.items[i].quality < 50) {
+                  this.items[i].quality = this.items[i].quality + 1;
+                }
               }
             }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
           }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+          if (this.items[i].sellIn < 0) {
+              if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+                this.items[i].quality = this.items[i].quality - this.items[i].quality;
+              }
+
+            } else {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1;
+              }
+            }
       }
     }
-
     return this.items;
+  }
+
+  _updateSellIn(item) {
+    if (!this._isSulfuras(item.name)) {
+      item.sellIn = item.sellIn - 1;
+    }
   }
 
   _isNormal(item_name) {
@@ -65,6 +75,20 @@ class Shop {
   _isSulfuras(item_name) {
     const validation = name =>
       item_name.search("Sulfuras") > -1 ? true : false;
+
+    return validation(item_name);
+  }
+
+  _isAgedBrie(item_name) {
+    const validation = name =>
+      item_name.search("Aged Brie") > -1 ? true : false;
+
+    return validation(item_name);
+  }
+
+  _isBackstagePasses(item_name) {
+    const validation = name =>
+      item_name.search("Backstage passes") > -1 ? true : false;
 
     return validation(item_name);
   }
