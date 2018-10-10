7f24a5bb16ba77795bda0526e20e5cac39bb8f42
davidlawes <davidlawes@hotmail.co.uk>
2018-10-10 17:13:30 +0100

    refactor: modified updateNormalItem in line with updateAgedBrie

--- js/src/UpdateNormalItem.js
+++ js/src/UpdateNormalItem.js
@@ -3,9 +3,8 @@
 (function(exports) {
   function updateNormalItem(item) {
     item.sellIn = item.sellIn - 1;
-    item.quality <= 0  ? item.quality = 0
-    : item.sellIn >= 0 ? item.quality -= 1
-                       : item.quality -= 2
+    item.sellIn >= 0  ? item.quality -= 1 : item.quality -= 2;
+    item.quality <= 0 ? item.quality = 0  : null;
     return item
   }
 
