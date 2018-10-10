9e94f8c3fbc5ba03091007d88f9dba1e3e71eb4f
davidlawes <davidlawes@hotmail.co.uk>
2018-10-10 19:12:33 +0100

    refactor: created truely private functions in Shop

--- js/src/shop.js
+++ js/src/shop.js
@@ -12,16 +12,16 @@
   Shop.prototype.updateQuality = function() {
     for (var i = 0; i < this.items.length; i++) {
       switch(true) {
-        case(this._isNormalItem(this.items[i].name)):
+        case(_isNormalItem.call(this, this.items[i].name)):
           this.updateNormalItem(this.items[i]);
           break;
-        case(this._isSpecialItem("Aged Brie", this.items[i].name)):
+        case(_isSpecialItem.call(this, "Aged Brie", this.items[i].name)):
           this.updateAgedBrie(this.items[i]);
           break;
-        case(this._isSpecialItem("Backstage passes", this.items[i].name)):
+        case(_isSpecialItem.call(this, "Backstage passes", this.items[i].name)):
           this.updateBackstagePasses(this.items[i]);
           break;
-        case(this._isSpecialItem("Conjured", this.items[i].name)):
+        case(_isSpecialItem.call(this, "Conjured", this.items[i].name)):
           this.updateConjured(this.items[i]);
           break;
       }
@@ -29,17 +29,17 @@
     return this.items;
   }
 
-  Shop.prototype._isNormalItem = function(item_name) {
+  function _isNormalItem(item_name) {
     const validation = name =>
-      this._isSpecialItem("Sulfuras", item_name)           ? false
-      : this._isSpecialItem("Aged Brie", item_name)        ? false
-      : this._isSpecialItem("Backstage passes", item_name) ? false
-      : this._isSpecialItem("Conjured", item_name)         ? false
-                                                           : true
+      _isSpecialItem.call(this, "Sulfuras", item_name)           ? false
+      : _isSpecialItem.call(this, "Aged Brie", item_name)        ? false
+      : _isSpecialItem.call(this, "Backstage passes", item_name) ? false
+      : _isSpecialItem.call(this, "Conjured", item_name)         ? false
+                                                                 : true
     return validation();
   }
 
-  Shop.prototype._isSpecialItem = function(special_name, item_name) {
+  function _isSpecialItem(special_name, item_name) {
     const validation = name =>
       item_name.search(special_name) > -1 ? true : false;
     return validation();
