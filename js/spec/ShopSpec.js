48db6e1463b8ffafa5eb8e4b279b8f61c42a065a
davidlawes <davidlawes@hotmail.co.uk>
2018-10-10 11:29:19 +0100

    test: added unit tests for Shop

--- js/spec/ShopSpec.js
+++ js/spec/ShopSpec.js
@@ -1,14 +1,38 @@
 describe("Shop", function() {
-  var items;
+  var item;
 
   describe(".updateQuality()", function() {
 
-    let item = new Item("Bread", 2, 3);
+    let item = {}
 
     it("returns an array of objects", function() {
       const gildedRose = new Shop([item]);
       const items = gildedRose.updateQuality();
       expect(items.constructor).toEqual(Array)
+    });
+
+    describe("given a normal item", function() {
+
+      beforeEach(function() {
+        item = new Item("Bread", 1, 1);
+        const gildedRose = new Shop([item]);
+        gildedRose.updateQuality();
+      });
+
+      it("reduces the sellIn value by 1", function() {
+        expect(item.sellIn).toEqual(0);
+      });
+
+      it("reduces the quality value by 1", function() {
+        expect(item.quality).toEqual(0);
+      });
+
+      it("caps quality at 0", function() {
+        const gildedRose = new Shop([item]);
+        gildedRose.updateQuality();
+        expect(item.quality).toEqual(0);
+      });
+
     });
 
   });
