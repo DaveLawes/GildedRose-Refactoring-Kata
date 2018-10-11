4bf59f662f21a932b56bafad81ed1784db529c2a
davidlawes <davidlawes@hotmail.co.uk>
2018-10-11 09:50:23 +0100

    test: all tests written for update.backstagePasses

--- js/spec/UpdateSpec.js
+++ js/spec/UpdateSpec.js
@@ -52,4 +52,38 @@ describe('Update', function() {
 
   });
 
+  describe('.backstagePasses()', function() {
+
+    it('decreases sellIn by 1', function() {
+      let item = { name: 'Backstage passes', sellIn: 1, quality: 4 };
+      expect(update.backstagePasses(item).sellIn).toEqual(0);
+    });
+
+    it('when sellIn > 10, increases the quality by 1', function() {
+      let item = { name: 'Backstage passes', sellIn: 12, quality: 4 };
+      expect(update.backstagePasses(item).quality).toEqual(5);
+    });
+
+    it('when 5 < sellIn <= 10, increases the quality by 2', function() {
+      let item = { name: 'Backstage passes', sellIn: 7, quality: 4 };
+      expect(update.backstagePasses(item).quality).toEqual(6);
+    });
+
+    it('when 0 <= sellIn <= 5, increases the quality by 3', function() {
+      let item = { name: 'Backstage passes', sellIn: 5, quality: 4 };
+      expect(update.backstagePasses(item).quality).toEqual(7);
+    });
+
+    it('caps highest quality at 50', function() {
+      let item = { name: 'Backstage passes', sellIn: 1, quality: 49 };
+      expect(update.backstagePasses(item).quality).toEqual(50);
+    });
+
+    it('when sellIn < 0, quality equals 0', function() {
+      let item = { name: 'Backstage passes', sellIn: 0, quality: 50 };
+      expect(update.backstagePasses(item).quality).toEqual(0);
+    });
+
+  });
+
 });
--- js/src/Update.js
+++ js/src/Update.js
@@ -6,7 +6,7 @@
   Update.prototype.normalItem = function(item) {
     _updateSellIn.call(this, item);
     item.sellIn >= 0  ? item.quality -= 1 : item.quality -= 2;
-    item.quality <= 0 ? item.quality = 0  : null;    
+    item.quality <= 0 ? item.quality = 0  : null;
     return item;
   };
 
@@ -14,6 +14,26 @@
     _updateSellIn.call(this, item);
     item.sellIn >= 0   ? item.quality += 1 : item.quality += 2;
     item.quality >= 50 ? item.quality = 50 : null;
+    return item;
+  };
+
+  Update.prototype.backstagePasses = function(item) {
+    _updateSellIn.call(this, item);
+    switch(true) {
+      case(item.sellIn < 0):
+        item.quality = 0;
+        break;
+      case(item.sellIn >=0 && item.sellIn <= 5):
+        item.quality += 3;
+        break;
+      case(item.sellIn > 5 && item.sellIn <= 10):
+        item.quality += 2;
+        break;
+      case(item.sellIn > 10):
+        item.quality += 1;
+        break;
+    }
+    item.quality > 50 ? item.quality = 50 : null;
     return item;
   };
 
