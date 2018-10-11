09ec6fd265da14e38fa73f6580e2bbe41b65ea10
davidlawes <davidlawes@hotmail.co.uk>
2018-10-11 09:44:16 +0100

    test: all tests written for update.agedBrie

--- js/spec/UpdateSpec.js
+++ js/spec/UpdateSpec.js
@@ -2,12 +2,28 @@
 
 describe('Update', function() {
 
-  describe('.updateSellIn', function() {
+  describe('.agedBrie', function() {
+
+    let update = new Update();
 
     it('decreases sellIn by 1', function() {
       let item = { name: 'Aged Brie', sellIn: 1, quality: 4 };
-      const update = new Update();
       expect(update.agedBrie(item).sellIn).toEqual(0);
+    });
+
+    it('increases quality by 1 when sellIn >= 0', function() {
+      let item = { name: 'Aged Brie', sellIn: 1, quality: 4 };
+      expect(update.agedBrie(item).quality).toEqual(5);
+    });
+
+    it('increases quality by 2 when sellIn < 0', function() {
+      let item = { name: 'Aged Brie', sellIn: 0, quality: 4 };
+      expect(updateAgedBrie(item).quality).toEqual(6);
+    });
+
+    it('caps highest quality at 50', function() {
+      let item = { name: 'Aged Brie', sellIn: 0, quality: 49 };
+      expect(updateAgedBrie(item).quality).toEqual(50);
     });
 
   });
--- js/src/Update.js
+++ js/src/Update.js
@@ -5,13 +5,14 @@
 
   Update.prototype.agedBrie = function(item) {
     _updateSellIn.call(this, item);
+    item.sellIn >= 0   ? item.quality += 1 : item.quality += 2;
+    item.quality >= 50 ? item.quality = 50 : null;
     return item;
   };
 
   function _updateSellIn(item) {
     item.sellIn -= 1;
   };
-
 
   exports.Update = Update;
 })(this);
